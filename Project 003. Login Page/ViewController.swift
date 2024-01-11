e3b54b95a3799e8403f1225f1d0239642a617a42
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-01-11 22:28:10 +0100

    Initial Commit

---
+++ Project 003. Login Page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 003. Login Page
+//
+//  Created by Mac Alexander on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
