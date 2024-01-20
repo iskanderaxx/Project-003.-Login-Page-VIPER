dce0a1ab43e9a64fe8788db68f77e5f452fa4577
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-01-20 19:48:11 +0100

    Preparing files for VIPER structure.

---
+++ Project 003. Login Page/Sources/VIPER Module/Presenter/LoginPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  LoginPresenter.swift
+//  Project 003. Login Page
+//
+//  Created by Mac Alexander on 19.01.2024.
+//
+
+import Foundation
---
+++ Project 003. Login Page/Sources/VIPER Module/View/LoginView.swift
@@ -0,0 +1,10 @@
+//
+//  ViewController.swift
+//  Project 003. Login Page
+//
+//  Created by Mac Alexander on 11.01.2024.
+//
+
+import Foundation
+import UIKit
+
